ec00322958243844b5e313ef65163c7d8efd9960
GitPoomCmu <152594220+GitPoomCmu@users.noreply.github.com>
2023-12-15 15:56:13 +0700

    Update lab9_1.cpp

--- lab9_1.cpp
+++ lab9_1.cpp
@@ -2,14 +2,16 @@
 #include<iomanip> //For using setw(), setprecision(), ...
 using namespace std;
 
-int main(){	
+int main(){
+    double loan, interest, pay;
+    
 	cout << "Enter initial loan: ";
+	cin >> loan;
 	cout << "Enter interest rate per year (%): ";
+	cin >> interest;
 	cout << "Enter amount you can pay per year: ";
+	cin >> pay;
 
-	//use 'setw' to set width of table and 'left' to set left-alignment
-	//you can change input argument of 'setw()' to see the effect
-	//Try to change from 'left' to 'right' and see the effect
 	cout << setw(13) << left << "EndOfYear#"; 
 	cout << setw(13) << left << "PrevBalance"; 
 	cout << setw(13) << left << "Interest"; 
@@ -18,16 +20,25 @@ int main(){
 	cout << setw(13) << left << "NewBalance";
 	cout << "\n";
 	
-	//use 'fixed' and 'setprecision' to fix the number of decimal digits for displaying
-	//you can change input argument of 'setprecision()' to see the effect
 	cout << fixed << setprecision(2); 
-	cout << setw(13) << left << 1; 
-	cout << setw(13) << left << 1000.0;
-	cout << setw(13) << left << 50.0;
-	cout << setw(13) << left << 1050.0;
-	cout << setw(13) << left << 100.0;
-	cout << setw(13) << left << 950.0;
-	cout << "\n";	
 	
+    for (int year = 1; ; year++) {
+	    cout << setw(13) << left << year; 
+	    cout << setw(13) << left << loan;
+	    
+	    cout << setw(13) << left << interest/100 * loan;
+	    loan += interest/100 * loan;
+	    cout << setw(13) << left << loan;
+	    
+	    pay = (pay>loan)?loan:pay;
+	    cout << setw(13) << left << pay;
+	    loan -= pay;
+	    
+            cout << setw(13) << left << loan;
+	    if(loan <= 0) break;
+	    
+	    cout << "\n";	
+    }
+    
 	return 0;
 }
